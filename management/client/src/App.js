e7d004f8b9031bd1725ae5a2826b32ddf1d0e311
Kw3c <shuttlecock@empas.com>
2021-11-11 01:50:38 +0900

    min-update

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,6 +18,9 @@ const styles = theme => ({
   },
   table: {
     minWidth: 786
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 })
 
@@ -48,6 +52,30 @@ const customers = [
 ]
 
 class App extends Component {
+
+  state = {
+    customers: "",
+    completed: 0
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 100);
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 })
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -65,13 +93,18 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {
-              customers.map(c => (
+              this.state.customers ? this.state.customers.map(c => (
                 <Customer
                   key={c.id} id={c.id} image={c.image}
                   name={c.name} birthday={c.birthday}
                   gender={c.gender} job={c.job}
                 />
-              ))
+              )) :
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                  </TableCell>
+                </TableRow>
             }
           </TableBody>
         </Table>
--- management/server.js
+++ management/server.js
@@ -1,13 +1,37 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      'id': 1,
+      'image': 'https://placeimg.com/64/64/1',
+      'name': '박보검',
+      'birthday': '1990.02.03',
+      'gender': '남자',
+      'job': '대학생'
+    },
+    {
+      'id': 2,
+      'image': 'https://placeimg.com/64/64/2',
+      'name': '조인성',
+      'birthday': '1985.04.13',
+      'gender': '남자',
+      'job': '무직'
+    },
+    {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/3',
+      'name': '공유',
+      'birthday': '1988.06.06',
+      'gender': '남자',
+      'job': '바리스타'
+    }
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
